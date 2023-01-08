72176dd94d2a9c552268d0e3c14f66d9da105a8f
Mikaela-D <G00401392@atu.ie>
2023-01-08 21:01:01 +0000

    Added new method delete

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -21,4 +21,16 @@ public class Calc {
         System.out.println("The total is " + total);
 
     }
+    public static void delete()
+    {
+        System.out.println("Please enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNum = inputs.nextInt();
+
+        System.out.println("Please enter your second number: ");
+        int secondNum = inputs.nextInt();
+
+        int delete = firstNum - secondNum;
+        System.out.println("The delNumb is : " + delete);
+    }
 }
