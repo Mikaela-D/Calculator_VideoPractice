49124e6a97b96aaa41a8f6810e7d506735a44eff
Mikaela-D <G00401392@atu.ie>
2023-01-08 21:02:00 +0000

    Added new method multiple

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -5,20 +5,11 @@ import java.util.Scanner;
 public class Calc {
 
     public static void main(String[] args) {
-        add();
+        multiple();
 
     }
-    public static void add()
+    public static void multiple()
     {
-        System.out.println("Please enter your first number :");
-        Scanner inputs = new Scanner(System.in);
-        int firstNumber = inputs.nextInt();
-
-        System.out.println("Please enter your second number :");
-        int secondNumber = inputs.nextInt();
-
-        int total = firstNumber + secondNumber;
-        System.out.println("The total is " + total);
 
     }
 }
